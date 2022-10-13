0713c6018d5c69ba08719efc22e6f8dccbac6575
Zhakhongir <ismailovzhakhongir@gmail.com>
2022-10-13 20:44:59 +0600

    added hw5

---
+++ src/components/Comment/Comment.js
@@ -0,0 +1,23 @@
+import "./Comment.css";
+import CommentDate from "./CommentDate";
+import CommentUserInfo from "./CommentUserInfo";
+import CommentWrapper from "./CommentWrapper";
+
+function Comment(props) {
+  return (
+    <div>
+      <CommentWrapper className="comment-first">
+        <CommentUserInfo
+          avatarUrl={props.data.author.avatarUrl}
+          name={props.data.author.name}
+        />
+      </CommentWrapper>
+
+      <CommentWrapper className="comment-second">
+        <div className="Comment-text">{props.data.text}</div>
+        <CommentDate date={props.data.date} />
+      </CommentWrapper>
+    </div>
+  );
+}
+export default Comment;
---
+++ src/components/Comment/CommentWrapper.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./CommentWrapper.css";
+
+function CommentWrapper(props) {
+  const classes = props.className + " wrapper"; //comment-first wrapper
+  return <div className={classes}>{props.children}</div>;
+}
+
+export default CommentWrapper;
+// 
